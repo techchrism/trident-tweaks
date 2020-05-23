4a2e2a04b67a10aaea020babe8787d734f7c1e67
TheTechdoodle <admin+github@darkender.com>
2020-05-23 10:19:14 -0700

    Add trident void saving

--- src/main/java/com/darkender/plugins/tridenttweaks/TridentTweaks.java
+++ src/main/java/com/darkender/plugins/tridenttweaks/TridentTweaks.java
@@ -1,6 +1,9 @@
 package com.darkender.plugins.tridenttweaks;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Trident;
@@ -11,16 +14,82 @@ import org.bukkit.event.player.PlayerPickupArrowEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+import org.bukkit.util.Vector;
+
+import java.util.HashMap;
+import java.util.UUID;
 
 public class TridentTweaks extends JavaPlugin implements Listener
 {
-    //TODO make this a config option
+    //TODO make these config options
     private boolean enableOffhandReturn = true;
+    private boolean enableVoidSaving = true;
+    
+    private BukkitTask voidSavingTask = null;
+    private HashMap<UUID, Block> platforms;
     
     @Override
     public void onEnable()
     {
+        platforms = new HashMap<>();
+        
         getServer().getPluginManager().registerEvents(this, this);
+        setupVoidSaving();
+    }
+    
+    private void setupVoidSaving()
+    {
+        if(enableVoidSaving && voidSavingTask == null)
+        {
+            voidSavingTask = (new BukkitRunnable()
+            {
+                @Override
+                public void run()
+                {
+                    for(World w : getServer().getWorlds())
+                    {
+                        for(Trident trident : w.getEntitiesByClass(Trident.class))
+                        {
+                            if(!platforms.containsKey(trident.getUniqueId()))
+                            {
+                                // Check if the trident is *about* to be in the void (possible to save it by placing blocks)
+                                Location futurePos = trident.getLocation().clone().add(trident.getVelocity());
+                                if(futurePos.getBlock().getType().isAir() && futurePos.getBlockY() < 5 && futurePos.getBlockY() >= 0)
+                                {
+                                    futurePos.getBlock().setType(Material.BARRIER);
+                                    platforms.put(trident.getUniqueId(), futurePos.getBlock());
+                                    // Remove the platform after 1 second (if it still exists)
+                                    getServer().getScheduler().scheduleSyncDelayedTask(TridentTweaks.this, new Runnable()
+                                    {
+                                        @Override
+                                        public void run()
+                                        {
+                                            if(platforms.containsKey(trident.getUniqueId()))
+                                            {
+                                                platforms.get(trident.getUniqueId()).setType(Material.AIR);
+                                                platforms.remove(trident.getUniqueId());
+                                            }
+                                        }
+                                    }, 20L);
+                                }
+                                else if(futurePos.getY() < 0)
+                                {
+                                    // If it's already in the void, shoot it upwards
+                                    trident.setVelocity(new Vector(0, 1, 0));
+                                }
+                            }
+                        }
+                    }
+                }
+            }).runTaskTimer(this, 1L, 1L);
+        }
+        else if(!enableVoidSaving && voidSavingTask != null)
+        {
+            voidSavingTask.cancel();
+            voidSavingTask = null;
+        }
     }
     
     @EventHandler
@@ -59,7 +128,7 @@ public class TridentTweaks extends JavaPlugin implements Listener
                 @Override
                 public void run()
                 {
-                    for(ItemStack i : p.getInventory())
+                    for(ItemStack i : p.getInventory().getContents())
                     {
                         // If we find the trident, put it in the offhand
                         if(i != null && i.equals(item))
